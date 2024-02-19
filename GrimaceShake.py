7c513bbb0c63dc902bbc72de79f3869dc603d767
Gay <arencg2007@gmail.com>
2024-02-19 14:19:00 -0500

    Added Gas Percentage - Stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -10,12 +10,19 @@ def gaslevelGauge():
     currentGasLevel = random.choice(gasLevelList)
     return currentGasLevel
 
-#Function that lists Gas Levels,randomly choosing one and returing it value
-def listofgasstations():
+#Function that lists Gas Stations,randomly choosing one and returing it value
+def listofgasStations():
     gasStations= ["Shell","Speedway","Marathon","Cicle k","Mobil","Costco","Meijer","7Eleven"]
     gasStationsNearby = random.choice(gasStations)
     return gasStationsNearby
 
+#Function will call the gasLevelGauge to determine our gas level and then locate a nearby gas station
+#by calling thw ListOfGasStations function if we are on Low or Quarter Tank
+def gasLevelAlert():
+    milesToGasStationsLow = random.uniform(1, 25)
+    milesToGasStationsQuarterTank = random.uniform(25.1, 50)
+    gasLevelGauge = gaslevelGauge()
+    print(milesToGasStationsLow)
+    print(milesToGasStationsQuarterTank)
 
-print(gaslevelGauge())
-print(listofgasstations())
+gasLevelAlert()
