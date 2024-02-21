6319bbfca4b1ef51a90bb0f8fbbedb8501e94219
ArenCG2007 <159182904+ArenCG2007@users.noreply.github.com>
2024-02-21 13:44:02 -0500

    Add elif for Low - Progammed in GitHub

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -27,6 +27,11 @@ def gasLevelAlert():
         print("***WARNING - YOU ARE ON EMPTY***\n")
         sleep(2.5)
         print("    ***Calling Triple AAA***")
+    elif gasLevelIndicator == "Low":
+        print("Your gas tank is extremely low,checking Google Maps for the nearest gas station")
+        sleep(2.5)
+        print("The closest gas station is ",listOfGasStations()"which is",milesToGasStationsLow,"miles away.")
+
 
 
 
